2143298d99b5353a39f4124f8c7680b78a921ac5
JomsTrinidad <joms.trinidad@gmail.com>
2025-09-28 17:58:05 +0800

    load

--- btre/settings.py
+++ btre/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig', #called from pages app using a class called: PagesConfig with a parameted called 'pages'
+    'listings.apps.ListingsConfig', 
+    'realtors.apps.RealtorsConfig',     
 ]
 
 MIDDLEWARE = [
@@ -62,10 +64,19 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#     #    'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Core2duo',
+        'HOST': 'localhost'
     }
 }
 
